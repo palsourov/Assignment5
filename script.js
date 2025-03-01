bd4309abb037f73a3617f14f80b977819f4ce853
palsourov <palsourov365@gmail.com>
2025-03-02 01:19:06 +0600

    update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@
     
 });
  
+// Background colour change
+ let color=['#F4F8D3','#FBF3B9','#B7B1F2','#B9B28A','#91AC8F']
+ let count=0;
+ const Background=document.getElementById("Background").addEventListener("click", function(event){
+    event.preventDefault();
+    count++;
+    if(count<color.length){ 
+        document.body.style.backgroundColor= color[count];
+    }
+    else if(count==color.length){
+        document.body.style.backgroundColor= '#F4F7FF'; 
+    }
+    else{
+        count=0;
+        document.body.style.backgroundColor= color[count]; 
+    }
+    
+ })
