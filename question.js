204519095e0282ae720b19368a8770ffd9071dfd
palsourov <palsourov365@gmail.com>
2025-03-02 00:36:41 +0600

    update index.html and script.js

---
+++ question.js
@@ -0,0 +1,4 @@
+document.getElementById("back").addEventListener("click",function(event){
+    event.preventDefault();
+      window.location.href="./index.html";  
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+ document.getElementById("question").addEventListener("click",function(event){
+    event.preventDefault();
+      window.location.href="./question.html";
+    
+});
+ 
